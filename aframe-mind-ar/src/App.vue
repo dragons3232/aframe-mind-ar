a051ad9f2029dfaadcf317ef547b01d57a53ef2a
long.phan <long.phan@netpower.no>
2023-06-07 00:42:57 +0700

    Add aframe io

--- aframe-mind-ar/src/App.vue
+++ aframe-mind-ar/src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <a-scene>
+    <a-assets>
+      <img id="netpower" src="netpower.png" />
+    </a-assets>
+
+    <a-entity id="camera" position="0 0 0" rotation="-20 0 0">
+      <a-camera
+        look-controls="enabled: true"
+        cursor="fuse: false; rayOrigin: mouse;"
+        raycaster="near: 0; far: 10"
+      />
+    </a-entity>
+
+    <a-box position="0 -1 -2" rotation="0 30 0" material="src: netpower.png" />
+    <a-sky color="#ECECEC"></a-sky>
+  </a-scene>
 </template>
 
 <script>
