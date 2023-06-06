6b82abeadd2d7ebe23d35ccedef902d46998ab53
long.phan <long.phan@netpower.no>
2023-06-07 01:31:02 +0700

    Flat target

--- aframe-mind-ar/src/App.vue
+++ aframe-mind-ar/src/App.vue
@@ -10,7 +10,7 @@
       <img id="netpower" src="netpower.png" />
     </a-assets>
 
-    <a-entity id="camera" position="0 0 0" rotation="-20 0 0">
+    <a-entity id="camera" position="0 0 0" rotation="0 0 0">
       <a-camera
         look-controls="enabled: true"
         cursor="fuse: false; rayOrigin: mouse;"
@@ -18,10 +18,13 @@
       />
     </a-entity>
 
-    <a-box
-      position="0 -1 -2"
-      rotation="0 30 0"
-      material="src: netpower.png"
+    <a-plane
+      color="blue"
+      opaciy="0.5"
+      position="0 0 0"
+      height="0.552"
+      width="1"
+      rotation="0 0 0"
       mindar-image-target="targetIndex: 0"
     />
   </a-scene>
